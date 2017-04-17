578b5a1792c63884c5ddcf8a7f2b481d6e5aa2de
George Zhang <gzhang01@college.harvard.edu>
2017-04-17 12:26:46 -0400

    Plotting losses

--- code/plot.py
+++ code/plot.py
@@ -30,24 +30,30 @@ w2 = np.matrix([0, 1, -0.5])
 w3 = np.matrix([0, 1, -0.5])
 
 # Granularity
-t = 5
+t = 50
 
-for _ in xrange(100 / t):
-	# Plot
+# Loss values
+xLoss = []
+sgdLoss = []
+nonPrivLoss = []
+objPretLoss = []
+
+for i in xrange(n ** 2 / t):
+	# Compiling data to plot
 	cat1 = []
 	cat2 = []
-	for i in xrange(data.shape[0]):
-		if labels.item(i, 0) == 1:
-			cat1.append((data.item(i, 0), data.item(i, 1)))
+	for j in xrange(data.shape[0]):
+		if labels.item(j, 0) == 1:
+			cat1.append((data.item(j, 0), data.item(j, 1)))
 		else:
-			cat2.append((data.item(i, 0), data.item(i, 1)))
+			cat2.append((data.item(j, 0), data.item(j, 1)))
 	xs = np.arange(0, 1.01, 0.05)
 	liney = [(-w_real[0] * x - w_real[2]) / w_real[1] for x in xs]
 	sgdy = [(-w1.item(0, 0) * x - w1.item(0, 2)) / w1.item(0, 1) for x in xs]
 	nonPrivY = [(-w2.item(0, 0) * x - w2.item(0, 2)) / w2.item(0, 1) for x in xs]
 	objPretY = [(-w3.item(0, 0) * x - w3.item(0, 2)) / w3.item(0, 1) for x in xs]
 
-
+	# Plotting data
 	plt.plot([pt[0] for pt in cat1], [pt[1] for pt in cat1], "go")
 	plt.plot([pt[0] for pt in cat2], [pt[1] for pt in cat2], "ro")
 	plt.plot(xs, liney, "blue")
@@ -55,15 +61,27 @@ for _ in xrange(100 / t):
 	plt.plot(xs, nonPrivY, "m")
 	plt.plot(xs, objPretY, "c")
 	plt.axis((0,1,0,1))
-	plt.show()
+	# plt.show()
+	plt.close()
 
 
 	# Run regression
-	w1 = logisticRegression(data, labels, eta=0.5, reg=0.001, t=t, w=w1)
-	w2 = privateLogReg(data, labels, eta=0.5, reg=0.001, t=t, eps=1, delta=0.1, c=400, w=w2)
-	w3 = objectivePerturbation(data, labels, eta=0.5, reg=0.001, t=t, eps=1, delta=0.1, w=w3)
+	w1, loss1 = logisticRegression(data, labels, eta=0.5, reg=0.001, t=t, w=w1)
+	w2, loss2 = privateLogReg(data, labels, eta=0.5, reg=0.001, t=t, eps=1, delta=0.1, c=400, w=w2)
+	w3, loss3 = objectivePerturbation(data, labels, eta=0.5, reg=0.001, t=t, eps=1, delta=0.1, w=w3)
 	print 1 / w1.item(0, 0) * w1
 	print 1 / w2.item(0, 0) * w2
 	print 1 / w3.item(0, 0) * w3
 	print w_real
+
+	# Add loss value
+	xLoss.append((i + 1) * t)
+	sgdLoss.append(loss1)
+	nonPrivLoss.append(loss2)
+	objPretLoss.append(loss3)
+
+plt.plot(xLoss, sgdLoss, "black")
+plt.plot(xLoss, nonPrivLoss, "m")
+plt.plot(xLoss, objPretLoss, "c")
+plt.show()
 
